991c44fdc83c99b762e79b78a316ae570c158a91
Dragomir Mildov <drashoo@abv.bg>
2018-10-01 00:01:04 +0300

    add new prop

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) =>{
-    res.send({hi: 'Hello'})
+    res.send({hi: 'Hello',ENV: 'dev'})
 })
 console.log('test dev')
 const PORT = process.env.PORT || 3333
