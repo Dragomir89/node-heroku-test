89459650ca05329eabe94abca36e9e4b5a61464e
Dragomir Mildov <drashoo@abv.bg>
2018-09-30 23:58:16 +0300

    test dev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/',(req,res) =>{
     res.send({hi: 'Hello'})
 })
-
+console.log('test dev')
 const PORT = process.env.PORT || 3333
 console.log(PORT)
 app.listen(PORT )
